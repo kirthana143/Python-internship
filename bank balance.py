295e859dd8c10715d7cdcf54fd3c9c0f5148b096
kirthana143 <10209kirthana.n@gmail.com>
2024-09-24 14:51:16 +0530

    Add files via upload

---
+++ bank balance.py
@@ -0,0 +1,6 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Rakshith\AppData\Local\Programs\Python\Python312\.g.py
+account balance is= 10000
+account balance is= 7500
