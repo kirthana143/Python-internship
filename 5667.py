f5c5113fad834c35e4cafa909ac3cc256c9f2f69
kirthana143 <10209kirthana.n@gmail.com>
2024-09-24 14:57:19 +0530

    Add files via upload

---
+++ 5667.py
@@ -0,0 +1,17 @@
+Days1={'Monday','Tuesday','Wednesday','Thursaday','Sunday'}
+Days2={'Friday','Saturday','Sunday'}
+print(Days1/Days2)
+
+
+
+
+
+
+
+
+
+
+a={1,2,3,4,5,6}
+b={1,2,9,8,10}
+c=a^b
+print(c)
